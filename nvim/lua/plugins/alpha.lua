20225e75fa83cb975f21838aa027c2a11136d640
Steinebeisser <paul.geisthardt@gmx.de>
2025-06-07 03:20:45 +0200

    change alpha start screen

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,12 +1,38 @@
+
 return {
-    "goolord/alpha-nvim",
-    -- dependencies = { 'echasnovski/mini.icons' },
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        local startify = require("alpha.themes.startify")
-        -- available: devicons, mini, default is mini
-        -- if provider not loaded and enabled is true, it will try to use another provider
-        startify.file_icons.provider = "devicons"
-        require("alpha").setup(startify.config)
-    end,
+	"goolord/alpha-nvim",
+	-- dependencies = { 'echasnovski/mini.icons' },
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		--local startify = require("alpha.themes.startify")
+		-- available: devicons, mini, default is mini
+		-- if provider not loaded and enabled is true, it will try to use another provider
+		--startify.file_icons.provider = "devicons"
+		--require("alpha").setup(startify.config)
+
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		dashboard.section.header.val = {
+			"   ▄████████     ███        ▄████████  ▄█  ███▄▄▄▄",
+			"  ███    ███ ▀█████████▄   ███    ███ ███  ███▀▀▀██▄",
+			"  ███    █▀     ▀███▀▀██   ███    █▀  ███▌ ███   ███",
+			"  ███            ███   ▀  ▄███▄▄▄     ███▌ ███   ███",
+			"▀███████████     ███     ▀▀███▀▀▀     ███▌ ███   ███",
+			"         ███     ███       ███    █▄  ███  ███   ███",
+			"   ▄█    ███     ███       ███    ███ ███  ███   ███",
+			" ▄████████▀     ▄████▀     ██████████ █▀    ▀█   █▀",
+		}
+
+		dashboard.section.buttons.val = {
+
+			--dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+			--dashboard.button("f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
+			--dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+			--dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+			--dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+		}
+
+		alpha.setup(dashboard.opts)
+	end,
 }
